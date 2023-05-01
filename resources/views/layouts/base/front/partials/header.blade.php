2ab9a127b73e8ced0c2c87ca6074f670721ffe4e
febryanpratama <spandupan46@student.untan.ac.id>
2023-05-01 17:34:04 +0700

    feat: rev header

--- resources/views/layouts/base/front/partials/header.blade.php
+++ resources/views/layouts/base/front/partials/header.blade.php
@@ -18,29 +18,33 @@
 				</div>
 				<div class="x_top_header_all_select_box_wrapper">
 					<ul>
-						<li class="language">
+						{{-- <li class="language">
 							<select class="myselect">
 								<option>EN</option>
 								<option>RO</option>
 								<option>IT</option>
 							</select> <i class="fa fa-globe"></i>
-						</li>
-						<li class="usd">
+						</li> --}}
+						{{-- <li class="usd">
 							<select class="myselect">
-								<option>USD</option>
+								<option>Register</option>
 								<option>EUR</option>
 								<option>CAD</option>
 							</select> <i class="fa fa-money"></i>
-						</li>
-						<li class="login"> <a href="login.html"><i class="fa fa-power-off"></i> &nbsp;&nbsp;login</a>
-						</li>
-						<li class="register"> <a href="register.html"><i class="fa fa-plus-circle"></i>
-								&nbsp;&nbsp;register</a>
+						</li> --}}
+						<li class="login"> <a href="{{ url('login') }}"><i class="fa fa-power-off"></i> &nbsp;&nbsp;login</a>
 						</li>
 						<li>
-							<button class="searchd"><i class="fa fa-search"></i>
-							</button>
+							<div class="dropdown-wrapper menu-button"> 
+								<a class="menu-button" href="#">Daftar</a>
+								<div class="drop-menu"> 
+									
+									<a class="menu-button" style="padding: 5px; text-align: center; color: black;font-size: 1rem" href="{{ url('auth/register') }}">Daftar Rental</a>
+									<a class="menu-button" style="padding: 5px; text-align: center; color: black;font-size: 1rem" href="{{ url('auth/register-customer') }}">Daftar Penyewa</a>
+								</div>
+							</div>
 						</li>
+						
 					</ul>
 				</div>
 			</div>
