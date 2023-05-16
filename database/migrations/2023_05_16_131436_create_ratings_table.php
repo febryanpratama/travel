1c6bacc75995a9a84f62a4b486230227ae465185
febryanpratama <spandupan46@student.untan.ac.id>
2023-05-16 20:18:14 +0700

    feat: add migration rating

---
+++ database/migrations/2023_05_16_131436_create_ratings_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('ratings', function (Blueprint $table) {
+            $table->id();
+            $table->integer('rental_id');
+            $table->integer('user_id');
+            $table->integer('mobil_id');
+            $table->integer('rating');
+            $table->string('review');
+            $table->softDeletes();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('ratings');
+    }
+};
